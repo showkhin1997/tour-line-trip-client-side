01ae248dfbb14ab1c64afbacc2aca4aa2f8baf20
showkhin1997 <showkhin15@gmail.com>
2021-10-29 14:59:58 +0600

    banner added

---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="mt-2 mb-5">
+            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="https://i.ibb.co/xGY4k8y/banner1.jpg" className="d-block w-100 h-75" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://i.ibb.co/ykT3gnw/banner2.jpg" className="d-block w-100 h-75" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://i.ibb.co/MZ5tfdF/banner3.jpg" className="d-block w-100 h-75" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h2>this is home</h2>
+            <h1 className="mb-4 banner-text-style">
+                <span className="me-3 word-wrap welcome-text-banner">WELCOME</span>
+                <span className="tour-text-banner word-wrap">Tour</span>
+                <span className="line-text-banner word-wrap">Line</span>
+                <span className="trip-text-banner word-wrap">Trip</span>
+            </h1>
+            <Banner></Banner>
         </div>
     );
 };
