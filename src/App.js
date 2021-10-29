f8374faaf4488c61a1797b63541b7bcae6d63af3
showkhin1997 <showkhin15@gmail.com>
2021-10-29 12:06:48 +0600

    navbar create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Shared/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
+import logo from '../../../images/logo.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <>
+                <Navbar sticky="top" variant="dark" collapseOnSelect expand="lg" className="navbar-style">
+                    <Container>
+                        <Navbar.Brand href="#home" className="me-5">
+                            <img
+                                alt=""
+                                src={logo}
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}
+                            <span className="tour-text">Toure</span>
+                            <span className="line-text tour-text">Line</span>
+                            <span className="trip-text tour-text">Trip</span></Navbar.Brand>
+                        <Navbar.Toggle />
+                        <Navbar.Collapse className="justify-content-end">
+                            <Nav.Link className="text-white">HOME</Nav.Link>
+                            <Nav.Link className="text-white">SERVICES</Nav.Link>
+                            <Nav.Link className="text-white">MY ORDERS</Nav.Link>
+                            <Nav.Link className="text-white">MANAGE ALL ORDERS</Nav.Link>
+                            <Nav.Link className="text-white">ADD NEW SERVICE</Nav.Link>
+                            <Nav.Link className="text-white">ABOUT US</Nav.Link>
+                            <Button className="logout-button" variant="light">Log out</Button>
+                            <Nav.Link className="login-button">Login</Nav.Link>
+                            <Navbar.Text className="text-light">
+                                Signed in as: <a href="#login">karim</a>
+                            </Navbar.Text>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
+        </div>
+    );
+};
+
+export default Header;
