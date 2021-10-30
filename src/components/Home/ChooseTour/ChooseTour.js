eb741bf1bb2b10ea93e8c43d204ff320b9e2159a
showkhin1997 <showkhin15@gmail.com>
2021-10-31 00:02:41 +0600

    classname change

--- src/components/Home/ChooseTour/ChooseTour.js
+++ src/components/Home/ChooseTour/ChooseTour.js
@@ -6,36 +6,36 @@ const ChooseTour = () => {
         <div className="offer-container">
             <h1 className="mt-5 mb-5 text-center section-title">Choose Your Plan</h1>
             <p className="text-center mt-1 w-50 mx-auto text-muted fst-italic">Find your next travel adventure and make it memorable. Explore wildlife, enjoy seaside or book a cruise tour. Check out our popular destinations.</p>
-            <div class="row row-cols-1 row-cols-md-2 g-3 mt-5 mb-5">
-                <div class="col">
-                    <div class="card  border-0">
-                        <i class="fas fa-plane font-awesome-icon"></i>
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Special</h5>
+            <div className="row row-cols-1 row-cols-md-2 g-3 mt-5 mb-5">
+                <div className="col">
+                    <div className="card  border-0">
+                        <i className="fas fa-plane font-awesome-icon"></i>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Flight</h5>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card border-0">
-                        <i class="fas fa-ship font-awesome-icon"></i>
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Crusises</h5>
+                <div className="col">
+                    <div className="card border-0">
+                        <i className="fas fa-ship font-awesome-icon"></i>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Crusises</h5>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card border-0">
-                        <i class="fas fa-hiking font-awesome-icon"></i>
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Advanture</h5>
+                <div className="col">
+                    <div className="card border-0">
+                        <i className="fas fa-hiking font-awesome-icon"></i>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Advanture</h5>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card border-0">
-                        <i class="fas fa-mountain font-awesome-icon"></i>
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Wildlife</h5>
+                <div className="col">
+                    <div className="card border-0">
+                        <i className="fas fa-mountain font-awesome-icon"></i>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Wildlife</h5>
                         </div>
                     </div>
                 </div>
