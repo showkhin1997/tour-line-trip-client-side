cd9f0b87bf720422ce0188ca826ddd41e53f291f
showkhin1997 <showkhin15@gmail.com>
2021-10-31 17:27:25 +0600

    subscribe button add

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -27,6 +27,7 @@ const Footer = () => {
                     <div className="col-sm mt-5">
                         <label htmlFor="subscribe" className="mb-2 subcribe-text">Subscribe</label><br />
                         <input className="input-subscribe" type="text" name="" id="" placeholder="Email" />
+                        <button className="btn btn logout-button mt-2 fw-bold">Subscribe</button>
                     </div>
                     <div className="col-sm mt-5"><small className="text-center text-mute">© 2021 Showkhin, All Rights Reserved</small></div>
                 </div>
